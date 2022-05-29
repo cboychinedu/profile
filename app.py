96bdcb8e34871bfd1ac936906b52b49f154da0a6
Mbonu Chinedum <cboy.chinedu@gmail.com>
2022-05-29 06:46:54 +0100

    worked on the projects mobile page

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def dated_url_for(endpoint, **values):
 # Running the flask application 
 # host = "192.168.196.200"
 if __name__ == "__main__":
-    app.run(port=5001, 
-            host="localhost",
-            debug=True)
-    # app.run() 
+    # app.run(port=5001, 
+    #         host="localhost",
+    #         debug=True)
+    app.run() 
